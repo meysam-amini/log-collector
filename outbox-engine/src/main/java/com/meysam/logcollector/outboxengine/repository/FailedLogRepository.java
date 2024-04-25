579ad93019981c0bcacb7141a92cc1b6dcffb955
meysam-amini <meysamamini7394@gmail.com>
2024-04-26 03:08:50 +0330

    FailedLogRepository

--- outbox-engine/src/main/java/com/meysam/logcollector/outboxengine/repository/FailedLogRepository.java
+++ outbox-engine/src/main/java/com/meysam/logcollector/outboxengine/repository/FailedLogRepository.java
@@ -4,11 +4,8 @@ import com.meysam.logcollector.common.model.entity.LogEntity;
 import com.meysam.logcollector.common.outbox.repository.OutboxRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface FailedLogRepository extends OutboxRepository<LogEntity> {
 
-    Optional<LogEntity> findByOutboxTrackingCode(int trackingCode);
 
 }
