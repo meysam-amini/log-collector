bd272ff561db7d6c4fddb9ff12979b83209ccf2a
meysam-amini <meysamamini7394@gmail.com>
2024-04-26 23:15:09 +0330

    Itests

--- auth/src/main/java/com/meysam/logcollector/auth/service/api/KeycloakService.java
+++ auth/src/main/java/com/meysam/logcollector/auth/service/api/KeycloakService.java
@@ -1,9 +1,9 @@
 package com.meysam.logcollector.auth.service.api;
 
 import com.meysam.logcollector.auth.model.enums.MemberLevel;
-import com.meysam.logcollector.common.model.dto.LoginRequestDto;
-import com.meysam.logcollector.common.model.dto.LoginResponseDto;
-import com.meysam.logcollector.common.model.dto.RegisterUserRequestDto;
+import com.meysam.logcollector.common.model.dtos.dto.LoginRequestDto;
+import com.meysam.logcollector.common.model.dtos.dto.LoginResponseDto;
+import com.meysam.logcollector.common.model.dtos.dto.RegisterUserRequestDto;
 import org.springframework.http.ResponseEntity;
 
 import java.lang.reflect.Member;
